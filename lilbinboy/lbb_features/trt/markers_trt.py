047bd405bbbf926f267044adb44c685e685e6a0e
Michael Jordan <michael@glowingpixel.com>
2024-10-26 16:06:03 -0400

    Movin

--- lilbinboy/lbb_features/trt/markers_trt.py
+++ lilbinboy/lbb_features/trt/markers_trt.py
@@ -1,6 +1,6 @@
 import dataclasses
 import avbutils
-from PySide6 import QtCore, QtGui
+from PySide6 import QtCore, QtGui, QtWidgets
 
 class LBMarkerIcons:
 
@@ -58,3 +58,69 @@ class LBMarkerIcon(QtGui.QIcon):
 		painter.end()
 
 		return pixmap
+	
+class LBMarkerPresetComboBox(QtWidgets.QComboBox):
+
+	sig_marker_preset_editor_requested = QtCore.Signal()
+	"""Request the editor"""
+
+	sig_marker_preset_changed = QtCore.Signal(str)
+	
+	def __init__(self, *args, **kwargs):
+
+		super().__init__(*args, **kwargs)
+
+		self._last_selected_preset_name = None
+		self._allow_edit_option = True
+
+		self.currentIndexChanged.connect(self.processSelection)
+
+	def setAllowEditOption(self, allow_editing:bool):
+		self._allow_edit_option = bool(allow_editing)
+	
+	def allowEditOption(self) -> bool:
+		return self._allow_edit_option
+	
+	def setMarkerPresets(self, marker_presets:dict[str, LBMarkerPreset]):
+
+		self.clear()
+
+		for preset_name, preset_data in marker_presets.items():
+			self.addItem(LBMarkerIcon(preset_data.color), preset_name, preset_data)
+
+		# Add edit option if it's there
+		if self.allowEditOption():
+			self.insertSeparator(len(marker_presets))
+			self.addItem("Add/Edit...")
+
+		self.setCurrentIndex(self.findData(self._last_selected_preset_name))
+	
+	@QtCore.Slot(int)
+	def processSelection(self, index:int):
+		"""Validate and process the pickins"""
+
+		if self.currentData() is not None:
+			self._last_selected_preset_name = self.currentData()
+			self.updateToolTip()
+			self.sig_marker_preset_changed.emit(self._last_selected_preset_name)
+
+		# If None (Edit selected), and it wasn't before, put the selection back to the previously-selected marker and request the editor
+		# I think this'll solve some recursive dialog boxes that do be happenin
+		elif self._last_selected_preset_name is not None and self.allowEditOption():
+			self.setCurrentIndex(self.findData(self._last_selected_preset_name))
+			self.sig_marker_preset_editor_requested.emit()
+
+	@QtCore.Slot()
+	def updateToolTip(self):
+
+		marker_preset = self.currentData()
+		
+		if marker_preset is None:
+			return "No Marker Preset Chosen"
+		
+		else:
+			return "Color: {color}; Comment: {comment}; Author: {author}".format(
+				color = marker_preset.color or "(Any)",
+				comment = ("\"" + marker_preset.comment + "\"") if marker_preset.comment else "(Any)",
+				author = ("\"" + marker_preset.author + "\"") if marker_preset.author else "(Any)",
+			)
--- lilbinboy/lbb_features/trt/panel_trt.py
+++ lilbinboy/lbb_features/trt/panel_trt.py
@@ -176,8 +176,8 @@ class TRTControlsTrims(TRTControls):
 		self._use_head_marker = QtWidgets.QCheckBox()
 		self._use_tail_marker = QtWidgets.QCheckBox()
 
-		self._from_head_marker = LBMarkerPresetComboBox()
-		self._from_tail_marker = LBMarkerPresetComboBox()
+		self._from_head_marker = markers_trt.LBMarkerPresetComboBox()
+		self._from_tail_marker = markers_trt.LBMarkerPresetComboBox()
 
 		self._icon_mark_in  = QtWidgets.QLabel(pixmap=QtGui.QPixmap(self.PATH_MARK_IN).scaledToHeight(16, QtCore.Qt.TransformationMode.SmoothTransformation))
 		self._icon_mark_out = QtWidgets.QLabel(pixmap=QtGui.QPixmap(self.PATH_MARK_OUT).scaledToHeight(16, QtCore.Qt.TransformationMode.SmoothTransformation))
@@ -228,73 +228,6 @@ class TRTControlsTrims(TRTControls):
 		"""Update FFOA and LFOA marker combo boxes"""
 		self._from_head_marker.setMarkerPresets(marker_presets)
 		self._from_tail_marker.setMarkerPresets(marker_presets)
-
-
-class LBMarkerPresetComboBox(QtWidgets.QComboBox):
-
-	sig_marker_preset_editor_requested = QtCore.Signal()
-	"""Request the editor"""
-
-	sig_marker_preset_changed = QtCore.Signal(str)
-	
-	def __init__(self, *args, **kwargs):
-
-		super().__init__(*args, **kwargs)
-
-		self._last_selected_preset_name = None
-		self._allow_edit_option = True
-
-		self.currentIndexChanged.connect(self.processSelection)
-
-	def setAllowEditOption(self, allow_editing:bool):
-		self._allow_edit_option = bool(allow_editing)
-	
-	def allowEditOption(self) -> bool:
-		return self._allow_edit_option
-	
-	def setMarkerPresets(self, marker_presets:dict[str, markers_trt.LBMarkerPreset]):
-
-		self.clear()
-
-		for preset_name, preset_data in marker_presets.items():
-			self.addItem(markers_trt.LBMarkerIcon(preset_data.color), preset_name, preset_data)
-
-		# Add edit option if it's there
-		if self.allowEditOption():
-			self.insertSeparator(len(marker_presets))
-			self.addItem("Add/Edit...")
-
-		self.setCurrentIndex(self.findData(self._last_selected_preset_name))
-	
-	@QtCore.Slot(int)
-	def processSelection(self, index:int):
-		"""Validate and process the pickins"""
-
-		if self.currentData() is not None:
-			self._last_selected_preset_name = self.currentData()
-			self.updateToolTip()
-			self.sig_marker_preset_changed.emit(self._last_selected_preset_name)
-
-		# If None (Edit selected), and it wasn't before, put the selection back to the previously-selected marker and request the editor
-		# I think this'll solve some recursive dialog boxes that do be happenin
-		elif self._last_selected_preset_name is not None and self.allowEditOption():
-			self.setCurrentIndex(self.findData(self._last_selected_preset_name))
-			self.sig_marker_preset_editor_requested.emit()
-
-	@QtCore.Slot()
-	def updateToolTip(self):
-
-		marker_preset = self.currentData()
-		
-		if marker_preset is None:
-			return "No Marker Preset Chosen"
-		
-		else:
-			return "Color: {color}; Comment: {comment}; Author: {author}".format(
-				color = marker_preset.color or "(Any)",
-				comment = ("\"" + marker_preset.comment + "\"") if marker_preset.comment else "(Any)",
-				author = ("\"" + marker_preset.author + "\"") if marker_preset.author else "(Any)",
-			)
 
 class LBTRTCalculator(LBUtilityTab):
 	"""TRT Calculator"""
