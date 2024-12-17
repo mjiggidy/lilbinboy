0f6ab320abca353c7075f1fa18b13d13a5750643
mjordan <michael@glowingpixel.com>
2024-12-16 23:23:00 -0800

    Just some thinking about errrors

--- lilbinboy/lbb_common/__init__.py
+++ lilbinboy/lbb_common/__init__.py
@@ -3,6 +3,33 @@ import avbutils
 from PySide6 import QtWidgets, QtCore, QtGui
 from timecode import Timecode
 
+class LBErrorLogWindow(QtWidgets.QWidget):
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.setWindowFlag(QtGui.Qt.WindowType.Tool)
+		self.setWindowTitle("Difficulties")
+		self.resize(400,260)
+
+
+		self.setLayout(QtWidgets.QVBoxLayout())
+		self.table_errors = LBErrorLogTableView()
+		self.layout().addWidget(self.table_errors)
+
+
+		self.error_detail = QtWidgets.QTextEdit()
+		self.error_detail.setReadOnly(True)
+		
+		
+		self.widgetmapper = QtWidgets.QDataWidgetMapper()
+		self.widgetmapper.setModel(self.table_errors.model())
+		self.widgetmapper.addMapping(self.error_detail, 2)
+		self.table_errors.selectionModel().currentRowChanged.connect(self.widgetmapper.setCurrentModelIndex)
+		#self.widgetmapper.toFirst()
+
+
+		self.layout().addWidget(self.error_detail)
+
 class LBUtilityTab(QtWidgets.QWidget):
 	"""Lil' Utility Container Boy"""
 
@@ -19,6 +46,29 @@ class LBMainTabs(QtWidgets.QTabWidget):
 		#self.addTab(LBUtilityTab(), "Lock Jockey")
 		#self.addTab(LBUtilityTab(), "Batch Bin Boy")
 
+class LBErrorLogTableView(QtWidgets.QTreeWidget):
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.setSelectionBehavior(QtWidgets.QTreeWidget.SelectionBehavior.SelectRows)
+		self.setSortingEnabled(True)
+		self.setIndentation(0)
+		self.setUniformRowHeights(True)
+		self.setAlternatingRowColors(True)
+		self.setHeaderLabels([
+			"Module",
+			"Datetime",
+			"Error Message"
+		])
+
+		self.addTopLevelItems([
+			QtWidgets.QTreeWidgetItem(["TRT Calculator", "Today", "Oh boy I don't even know where to get started"]),
+			QtWidgets.QTreeWidgetItem(["TRT Calculator", "Today", "There were markers I didn't know how to read"]),
+			QtWidgets.QTreeWidgetItem(["TRT Calculator", "Today", "That bin was just too scary to be honest"]),
+		])
+
+		for col in range(0, self.columnCount()):
+			self.resizeColumnToContents(col)
 
 
 class LBMainWindow(QtWidgets.QMainWindow):
@@ -44,9 +94,20 @@ class LBMainWindow(QtWidgets.QMainWindow):
 		self.lbl_lbbicon = QtWidgets.QLabel()
 		self.lbl_lbbicon.setPixmap(QtGui.QPixmap("lilbinboy/logos/256x256.png").scaledToHeight(48))
 
+		self.btn_errorlog = QtWidgets.QPushButton("Show Error Log")
+		self.btn_errorlog.clicked.connect(self.errorLogRequested)
+
 		lay_id.addWidget(self.lbl_lbbicon)
+		lay_id.addWidget(self.btn_errorlog)
+		lay_id.addStretch()
 		lay_id.addWidget(self.lbl_lbb)
 		self.centralWidget().layout().addLayout(lay_id)
+	
+	@QtCore.Slot()
+	def errorLogRequested(self):
+		wnd_errors = LBErrorLogWindow(self)
+		
+		wnd_errors.show()
 
 	def moveEvent(self, event):
 		super().moveEvent(event)
