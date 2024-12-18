7dd2ff1b2371a8f08d3328e39eb1962d1e9cd2f6
mjordan <michael@glowingpixel.com>
2024-12-18 00:06:58 -0800

    setup

--- setup.py
+++ setup.py
@@ -1,7 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="lilbinboy",
-    version="0.1.0",
-    packages=find_packages(),
+	name="lilbinboy",
+	version="0.1.0",  # Update as needed
+	description="Lil' Bin Boy will do it!",
+	long_description=open("README.md").read(),
+	long_description_content_type="text/markdown",
+	author="Michael Jordan",
+	author_email="michael@glowingpixel.com",
+	url="https://github.com/mjiggidy/lilbinboy",  # Replace with your repository URL
+	packages=find_packages(include=["lilbinboy", "lilbinboy.*"]),
+	install_requires=[],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",  # Update if using a different license
+		"Operating System :: OS Independent",
+	],
+	python_requires=">=3.7",
+	include_package_data=True,
+	entry_points={
+		"console_scripts": [
+			"lilbinboy=lilbinboy.__main__:main",  # Updated to use __main__.py as the entry point
+		],
+	},
 )
