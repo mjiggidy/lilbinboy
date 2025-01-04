f44e144c983587c31dc71d80525ee9edb24f6891
Michael Jordan <michael@glowingpixel.com>
2025-01-03 19:36:48 -0800

    Initial DMG script

---
+++ build_tools/mac_create_dmg.sh
@@ -0,0 +1,12 @@
+create-dmg \
+	--volname "Lil' Bin Boy!" \
+	--volicon "build_tools/icons/macos_lilbinboy.icns" \
+	--window-pos 200 120 \
+	--window-size 500 250 \
+	--icon-size 512 \
+	--icon "build/Lil' Bin Boy.app" 100 50 \
+	--app-drop-link 600 50 \
+	--skip-jenkins \
+	--eula "EULA"\
+	"Application-Installer.dmg" \
+	"build/"
