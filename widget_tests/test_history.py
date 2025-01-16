6ef7a83f9329d2dd9d39f114e4c66d3160203aa2
Michael Jordan <michael@glowingpixel.com>
2025-01-16 12:51:45 -0800

    In progress

--- widget_tests/test_history.py
+++ widget_tests/test_history.py
@@ -5,7 +5,6 @@ from lilbinboy.lbb_features.trt.hist_main import TRTHistoryViewer
 app = QtWidgets.QApplication()
 app.setStyle("Fusion")
 
-wnd_history = TRTHistoryViewer()
 
 db = QtSql.QSqlDatabase.addDatabase("QSQLITE", "trt")
 db.setDatabaseName(sys.argv[1])
@@ -14,13 +13,11 @@ db.open()
 if not db.open():
 	print("Nah lol", db.lastError().text())
 
-query_labels = QtSql.QSqlQuery("SELECT * FROM trt_snapshot_labels ORDER BY is_current DESC, datetime_created DESC", QtSql.QSqlDatabase.database("trt"))
-wnd_history._lst_saved.model().setQuery(query_labels)
-
 
 #query_sequences = QtSql.QSqlQuery(QtSql.QSqlDatabase.database("trt"))
 #wnd_history._temp_history_panel.model().setQuery(query_sequences)
 
+wnd_history = TRTHistoryViewer()
 wnd_history.updateLiveSnapshot([])
 wnd_history.saveLiveToSnapshot("Taa Haa!!")
 wnd_history.show()
