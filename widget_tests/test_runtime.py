3969927ed5a6943834fd4daba83a3ff2c9d12da5
Michael Jordan <michael@glowingpixel.com>
2025-04-06 13:56:57 -0700

    Stylesheet

--- widget_tests/test_runtime.py
+++ widget_tests/test_runtime.py
@@ -2,6 +2,7 @@ from PySide6 import QtWidgets
 from lilbinboy.lbb_features.trt.panel_runtime import LBBRuntimeMetrics
 
 app = QtWidgets.QApplication()
+app.setStyle("Fusion")
 
 wnd = LBBRuntimeMetrics()
 wnd.show()
