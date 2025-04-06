7691f14284bbd16f1808bb1750247fc0973c9afc
Michael Jordan <michael@glowingpixel.com>
2025-04-06 14:27:17 -0700

    Initial bare setup

--- lilbinboy/lbb_features/trt/panel_runtime.py
+++ lilbinboy/lbb_features/trt/panel_runtime.py
@@ -1,9 +1,9 @@
 from PySide6 import QtCore, QtGui, QtWidgets
-from . import treeview_trt, wdg_summary, trims_trt
+from . import treeview_trt, wdg_stats, trims_trt
 from .panel_trt import TRTBinLoadingProgressBar, TRTModeSelection
-from lilbinboy.lbb_common import LBTimelineView, LBSpinBoxTC
+from ...lbb_common import LBTimelineView, LBSpinBoxTC
 
-class LBBRuntimeMetrics(QtWidgets.QWidget):
+class LBBRuntimeMetricsPanel(QtWidgets.QWidget):
 
 	def __init__(self, *args, **kwargs):
 
@@ -23,7 +23,7 @@ class LBBRuntimeMetrics(QtWidgets.QWidget):
 
 		# Declare main treeview
 		self.tree_sequenceview = treeview_trt.TRTTreeView()
-		self.trt_summary = wdg_summary.TRTSummary()
+		self.stats_trt = wdg_stats.TRTStatView()
 
 		# Longplay view
 		self.timeline_longplay = LBTimelineView()
@@ -36,55 +36,67 @@ class LBBRuntimeMetrics(QtWidgets.QWidget):
 		self.btn_export_data = QtWidgets.QPushButton()
 		self.btn_snapshots = QtWidgets.QPushButton()
 
-		self.setupWidgets()
+		self._setupWidgets()
 	
-	def setupWidgets(self):
+	def _setupWidgets(self):
 
-		# Setup top controls
-		lay_loader_controls = QtWidgets.QHBoxLayout()
-		
+		# ---
+		# Top controls
 		self.btn_add_bins.setText("Add From Bins...")
 		self.btn_add_bins.setToolTip("Add the latest sequence(s) from one or more bins")
 		self.btn_add_bins.setIcon(QtGui.QIcon.fromTheme(QtGui.QIcon.ThemeIcon.ListAdd))
-		lay_loader_controls.addWidget(self.btn_add_bins)
-		
+
 		# Stacked widget for progress vs sequence selection
 		self.stack_bin_loading.setSizePolicy(QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Policy.MinimumExpanding, QtWidgets.QSizePolicy.Policy.Maximum))
 		self.stack_bin_loading.addWidget(self.prog_loading)
 		self.stack_bin_loading.addWidget(self.bin_mode)
 		self.stack_bin_loading.setCurrentWidget(self.bin_mode)
-		lay_loader_controls.addWidget(self.stack_bin_loading)
 
-		# Refresh/Clear controls
+		# Refresh & Clear controls
 		self.btn_refresh_bins.setToolTip("Reload the existing bins for updates")
 		self.btn_refresh_bins.setIcon(QtGui.QIcon.fromTheme(QtGui.QIcon.ThemeIcon.ViewRefresh))
-		lay_loader_controls.addWidget(self.btn_refresh_bins)
 
 		self.btn_clear_bins.setToolTip("Clear the existing sequences")
 		self.btn_clear_bins.setIcon(QtGui.QIcon.fromTheme(QtGui.QIcon.ThemeIcon.EditClear))
+		
+		lay_loader_controls = QtWidgets.QHBoxLayout()
+		lay_loader_controls.addWidget(self.btn_add_bins)
+		lay_loader_controls.addWidget(self.stack_bin_loading)
+		lay_loader_controls.addWidget(self.btn_refresh_bins)
 		lay_loader_controls.addWidget(self.btn_clear_bins)
 
 		self.layout().addLayout(lay_loader_controls)
 
-		# Setup main treeview
+		# ---
+		# Main treeview
 		# NOTE: Go back and get fit_headers(), header() context menu setup, need to set model
 		self.layout().addWidget(self.tree_sequenceview)
 
+		# ---
 		# Longplay view
 		self.layout().addWidget(self.timeline_longplay)
 
+		# ---
+		# Stats/Summary View
+		grp_stats = QtWidgets.QGroupBox()
+		grp_stats.setLayout(QtWidgets.QVBoxLayout())
+		grp_stats.layout().setContentsMargins(0,0,0,0)
+		grp_stats.layout().addWidget(self.stats_trt)
+		self.layout().addWidget(grp_stats)
 
-		# TODO: Summaries
-
+		# ---
 		# Exporters
-		lay_exporters = QtWidgets.QHBoxLayout()
-		
-		lay_exporters.addWidget(self.btn_export_data)
+		self.btn_export_data.setText("Export As...")
+		self.btn_export_data.setIcon(QtGui.QIcon.fromTheme(QtGui.QIcon.ThemeIcon.DocumentSaveAs))
 
 		self.btn_snapshots.setText("Show Snapshots...")
 		self.btn_snapshots.setIcon(QtGui.QIcon.fromTheme(QtGui.QIcon.ThemeIcon.ViewRestore))
+		
+		lay_exporters = QtWidgets.QHBoxLayout()
+		lay_exporters.addWidget(self.btn_export_data)
 		lay_exporters.addWidget(self.btn_snapshots)
 		self.layout().addLayout(lay_exporters)
 		
-		# Set up sequence trim controls
+		# ---
+		# Trim controls
 		self.layout().addWidget(self.trt_trims)
