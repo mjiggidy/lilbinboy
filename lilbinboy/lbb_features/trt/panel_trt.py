97aba927925c7066cc9eb256172beaacb59a27d2
Michael Jordan <michael@glowingpixel.com>
2025-07-16 15:07:48 -0700

    Never mind bout those relative imports

--- lilbinboy/lbb_features/trt/panel_trt.py
+++ lilbinboy/lbb_features/trt/panel_trt.py
@@ -1,8 +1,8 @@
 from PySide6 import QtWidgets, QtGui, QtCore, QtSql
 from timecode import Timecode
 from concurrent import futures
-from ...lbb_common import LBUtilityTab, LBSpinBoxTC, LBTimelineView
-from . import dlg_choose_columns, dlg_marker, logic_trt, model_trt, markers_trt, dlg_sequence_selection, dlg_choose_columns, exporters_trt, wdg_sequence_treeview, wdg_sequence_trims, wdg_stats, hist_main
+from lilbinboy.lbb_common import LBUtilityTab, LBSpinBoxTC, LBTimelineView, LBSettingsManager
+from lilbinboy.lbb_features.trt import dlg_choose_columns, dlg_marker, logic_trt, model_trt, markers_trt, dlg_sequence_selection, dlg_choose_columns, exporters_trt, wdg_sequence_treeview, wdg_sequence_trims, wdg_stats, hist_main
 from .settings_keys import TRTSettingsKeys
 
 
@@ -257,6 +257,7 @@ class LBTRTCalculator(LBUtilityTab):
 		
 		self.model().set_active_head_marker_preset_name(self.settingsManager().value(TRTSettingsKeys.TRIM_HEAD_MARKER))
 		self.model().set_active_tail_marker_preset_name(self.settingsManager().value(TRTSettingsKeys.TRIM_TAIL_MARKER))
+
 
 		self.setFieldVisibility(self.settingsManager().value(TRTSettingsKeys.LIST_FIELDS_ORDER, [], type=list), self.settingsManager().value(TRTSettingsKeys.LIST_FIELDS_HIDDEN, [], type=list))
 		
