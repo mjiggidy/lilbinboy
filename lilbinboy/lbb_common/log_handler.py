9bc47800e0052c91b576e55c5bd6f41dff96840d
Michael Jordan <michael@glowingpixel.com>
2025-08-07 11:29:32 -0400

    Cap max records

--- lilbinboy/lbb_common/log_handler.py
+++ lilbinboy/lbb_common/log_handler.py
@@ -6,11 +6,17 @@ class LBLogDataModel(QtCore.QAbstractItemModel):
 	"""Qt Data model for Lil' Gui' Loggin Boy"""
 
 	HEADERS:list[str] = ["Module","Timestamp","Message"]
+	"""Headers available for display"""
 
-	def __init__(self, *args, **kwargs):
+	DEFAULT_MAX_RECORDS:int = 128
+	"""Default number of maxmimum records unless explicitly set per instance"""
+
+	def __init__(self, max_records:int=DEFAULT_MAX_RECORDS, *args, **kwargs):
 
 		super().__init__(*args, **kwargs)
+		
 		self._records:list[logging.LogRecord] = []
+		self._max_records = max(int(max_records), 0)
 	
 	def addLogRecord(self, record:logging.LogRecord):
 		"""Add a log record"""
@@ -19,30 +25,62 @@ class LBLogDataModel(QtCore.QAbstractItemModel):
 		self._records.insert(0, record)
 		self.endInsertRows()
 
+		self.cullRecords()
+	
+	def cullRecords(self) -> int:
+		"""Keep record count below max length"""
+
+		extra_records = max(len(self._records) - self._max_records, 0)
+
+		if extra_records:
+			self.beginRemoveRows(QtCore.QModelIndex(), self._max_records, self._max_records + extra_records)
+			self._records = self._records[:self._max_records]
+			self.endRemoveRows()
+
+		# Return how many records have been yeeted gracefully	
+		return extra_records
+	
+	def setMaxRecords(self, max_records:int, delay_cull:bool=False):
+		"""Set a new max record length and cull extras"""
+
+		if self._max_records < 0:
+			raise ValueError("Max records cannot be negative")
+
+		self._max_records = int(max_records)
+		
+		# Trim off them extras real good. No room for you fellas!
+		if not delay_cull:
+			self.cullRecords()
+	
+	def maxRecords(self) -> int:
+		"""Maximum number of log records maintained"""
+		
+		return self._max_records
+
 	def parent(self, child:QtCore.QModelIndex) -> QtCore.QModelIndex:
+		"""Return the parent index (will be invalid; keepin' it flat)"""
+		# Keepin' it flat for now
 		return QtCore.QModelIndex()
 	
 	def rowCount(self, /, parent:QtCore.QModelIndex=None) -> int:
-		
-		# Keep er flat
+		"""Return the number of records"""
+
+		# Keep 'er flat
 		if parent.isValid():
 			return 0
 			
 		return len(self._records)
-
 		
 	def columnCount(self, /, parent:QtCore.QModelIndex=None) -> int:
+		"""Return the number of column headers"""
 
 		return len(self.HEADERS)
 	
 	def data(self, index:QtCore.QModelIndex, /, role:QtCore.Qt.ItemDataRole):
-		
-		#if index.parent().isValid():
-		#		return None
-		
+		"""Return the requested data for an index"""
+
 		if role == QtCore.Qt.ItemDataRole.DisplayRole:
 			return self.getLogRecordAttribute(log_index=index.row(), log_attribute=index.column())
-
 		
 	def headerData(self, section:int, orientation:QtCore.Qt.Orientation, /, role:QtCore.Qt.ItemDataRole=None):
 		
@@ -52,6 +90,9 @@ class LBLogDataModel(QtCore.QAbstractItemModel):
 		#return super().headerData(section, orientation, role)
 	
 	def getLogRecordAttribute(self, log_index:int, log_attribute:int):
+		"""Get a record"""
+		# TODO: Needs work
+
 		record = self._records[log_index]
 
 		if log_attribute == 0:
@@ -63,22 +104,22 @@ class LBLogDataModel(QtCore.QAbstractItemModel):
 	
 	def index(self, row:int, column:int, /, parent:QtCore.QModelIndex=QtCore.QModelIndex()):
 		return self.createIndex(row, column)
-
 	
 
-
 class LBLogHandler(logging.Handler):
-	"""A python `logging` handler for Lil' GUI Boy"""
+	"""
+	A python `logging` handler for Lil' GUI Boy.
+	Hands off `logging.LogRecord`s to `LBLogDataModel` for PySide6 model/view stuff
+	"""
 
 	def __init__(self, data_model:LBLogDataModel, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self._data_model = data_model
 
 	def data_model(self) -> LBLogDataModel:
+		"""Return the associated `LBLogDataModel` instance"""
 		return self._data_model
 	
-
 	def emit(self, record:logging.LogRecord):
-		print("OH HI")
+		"""Do a log real nice"""
 		self.data_model().addLogRecord(record)
-		#return super().emit(record)
