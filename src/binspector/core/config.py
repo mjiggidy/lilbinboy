eb55a48ef7039581975d8bdb49bc4c77d1acfd0d
Michael Jordan <michael@glowingpixel.com>
2026-05-27 12:09:02 -0700

    Bump

--- src/binspector/core/config.py
+++ src/binspector/core/config.py
@@ -7,7 +7,7 @@ class BSApplicationConfig:
 	APPLICATION_NAME    = "Binspector"
 	"""Name o' the app"""
 
-	APPLICATION_VERSION = QtCore.QVersionNumber(0,0,23)
+	APPLICATION_VERSION = QtCore.QVersionNumber(0,0,24)
 	"""Version Number (major.minor.revision)"""
 
 	APPLICATION_STORAGE_PATH = QtCore.QStandardPaths.StandardLocation.AppDataLocation
