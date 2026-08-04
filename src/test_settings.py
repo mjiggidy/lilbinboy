90bbd81e6e5c48760c7f8c879677fea2cf92c26d
Michael Jordan <michael@glowingpixel.com>
2026-08-04 13:26:36 -0700

    Nicer layout

--- src/test_settings.py
+++ src/test_settings.py
@@ -5,22 +5,28 @@ from lilbinboy.settingseditor import panel_binloading, panel_startup, panel_ui
 app = QtWidgets.QApplication()
 app.setStyle("Fusion")
 
-wnd_settings = QtWidgets.QTabWidget()
-wnd_settings.setWindowFlag(QtCore.Qt.WindowType.Dialog|QtCore.Qt.WindowType.Tool)
+wnd_settings = QtWidgets.QWidget()
+wnd_settings.setWindowFlag(QtCore.Qt.WindowType.Tool)
+wnd_settings.setWindowTitle("Settings")
 
-wnd_settings.setUsesScrollButtons(False)
+tabs_settings = QtWidgets.QTabWidget()
+
+tabs_settings.setUsesScrollButtons(False)
 
 pnl_binloading = panel_binloading.BSSettingsBinLoading()
 pnl_startup    = panel_startup.BSSettingsStartup()
 pnl_ui         = panel_ui.BSSettingsUserInterface()
 
-wnd_settings.addTab(pnl_startup, "Startup")
-wnd_settings.addTab(pnl_binloading,"Bin Loading")
-wnd_settings.addTab(pnl_ui, "User Interface")
+tabs_settings.addTab(pnl_startup, "Startup")
+tabs_settings.addTab(pnl_binloading,"Bin Loading")
+tabs_settings.addTab(pnl_ui, "User Interface")
 
-wnd_settings.setTabIcon(0, QtGui.QIcon.fromTheme(QtGui.QIcon.ThemeIcon.CallStart))
-wnd_settings.setTabIcon(1, QtGui.QIcon.fromTheme(QtGui.QIcon.ThemeIcon.ImageLoading))
-wnd_settings.setTabIcon(2, QtGui.QIcon.fromTheme(QtGui.QIcon.ThemeIcon.WindowNew))
+tabs_settings.setTabIcon(0, QtGui.QIcon.fromTheme(QtGui.QIcon.ThemeIcon.CallStart))
+tabs_settings.setTabIcon(1, QtGui.QIcon.fromTheme(QtGui.QIcon.ThemeIcon.ImageLoading))
+tabs_settings.setTabIcon(2, QtGui.QIcon.fromTheme(QtGui.QIcon.ThemeIcon.WindowNew))
+
+wnd_settings.setLayout(QtWidgets.QVBoxLayout())
+wnd_settings.layout().addWidget(tabs_settings)
 
 wnd_settings.show()
 
