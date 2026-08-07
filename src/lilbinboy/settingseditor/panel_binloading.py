22de35f57133cdb23101fbc6555d21177e24cb80
Michael Jordan <michael@glowingpixel.com>
2026-08-06 19:16:01 -0700

    Fixed width

--- src/lilbinboy/settingseditor/panel_binloading.py
+++ src/lilbinboy/settingseditor/panel_binloading.py
@@ -6,3 +6,19 @@ class BSSettingsBinLoading(QtWidgets.QWidget):
 	def __init__(self, *args, **kwargs):
 
 		super().__init__(*args, **kwargs)
+
+		self.setLayout(QtWidgets.QVBoxLayout())
+
+		self._chk_bin_view = QtWidgets.QCheckBox()
+		self._chk_bin_theme = QtWidgets.QCheckBox()
+		self._chk_bin_viewmode = QtWidgets.QCheckBox()
+		self._chk_bin_display = QtWidgets.QCheckBox()
+		self._chk_column_widths = QtWidgets.QCheckBox()
+
+		self._sld_queue_size = QtWidgets.QSlider()
+
+		self.setupWidgets()
+
+	def setupWidgets(self):
+
+		pass
