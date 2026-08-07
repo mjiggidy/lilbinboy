b6f182391b17eb26845d4426f9cd4e28c4e8b9de
Michael Jordan <michael@glowingpixel.com>
2026-08-06 19:13:59 -0700

    We reactive now

--- src/lilbinboy/settingseditor/panel_ui.py
+++ src/lilbinboy/settingseditor/panel_ui.py
@@ -15,6 +15,11 @@ DEFAULT_FANCY_ANIMATIONS = True
 class BSSettingsUserInterface(QtWidgets.QWidget):
 	"""User Interface Options Panel"""
 
+	sig_scroll_scale_changed = QtCore.Signal(int)
+	sig_list_padding_changed = QtCore.Signal(QtCore.QMargins)
+	sig_tools_follow_window_changed = QtCore.Signal(bool)
+	sig_use_fancy_animations_changed = QtCore.Signal(bool)
+
 	def __init__(self, *args, **kwargs):
 
 		super().__init__(*args, **kwargs)
@@ -32,6 +37,7 @@ class BSSettingsUserInterface(QtWidgets.QWidget):
 		self._chk_fancy_animation = QtWidgets.QCheckBox()
 
 		self.setupWidgets()
+		self.setupSignals()
 
 	def setupWidgets(self):
 
@@ -44,7 +50,7 @@ class BSSettingsUserInterface(QtWidgets.QWidget):
 		self._sld_scroll_scale.setValue(DEFAULT_BOTTOM_SCALE)
 		self._sld_scroll_scale.setOrientation(QtCore.Qt.Orientation.Horizontal)
 
-		self._lbl_scroll_scale.setMinimumWidth(self._lbl_scroll_scale.fontMetrics().averageCharWidth() * 4)
+		self._lbl_scroll_scale.setMinimumWidth(self._lbl_scroll_scale.fontMetrics().maxWidth() * 3)
 		self._lbl_scroll_scale.setAlignment(QtCore.Qt.AlignmentFlag.AlignRight|QtCore.Qt.AlignmentFlag.AlignVCenter)
 		self._lbl_scroll_scale.setText(f"{self._sld_scroll_scale.value()}%")
 
@@ -102,7 +108,7 @@ class BSSettingsUserInterface(QtWidgets.QWidget):
 		self._chk_tools_follow.setChecked(DEFAULT_TOOLS_FOLLOW)
 
 		lay_tools_follow = QtWidgets.QHBoxLayout()
-		lay_tools_follow.addWidget(QtWidgets.QLabel(self.tr("Tools follow main window:")))
+		lay_tools_follow.addWidget(QtWidgets.QLabel(self.tr("Tools follow main window position:")))
 		lay_tools_follow.addStretch()
 		lay_tools_follow.addWidget(self._chk_tools_follow)
 
@@ -134,3 +140,41 @@ class BSSettingsUserInterface(QtWidgets.QWidget):
 		grp_fancy_animations.layout().addWidget(lbl_fancy_animations)
 
 		self.layout().addWidget(grp_fancy_animations)
+
+	def setupSignals(self):
+
+		self._sld_scroll_scale.valueChanged.connect(self._userSelectedBottomScale)
+
+		self._spn_list_pad_w.valueChanged.connect(self._userChangedListPadding)
+		self._spn_list_pad_h.valueChanged.connect(self._userChangedListPadding)
+
+		self._chk_tools_follow.checkStateChanged.connect(self._userChangedToolsFollow)
+		self._chk_fancy_animation.checkStateChanged.connect(self._userChangedFancyAnimations)
+
+	@QtCore.Slot(int)
+	def _userSelectedBottomScale(self, value:int):
+
+		self._lbl_scroll_scale.setText(str(value) + "%")
+		self.sig_scroll_scale_changed.emit(value)
+
+	@QtCore.Slot()
+	def _userChangedListPadding(self):
+
+		list_padding = QtCore.QMargins(
+			self._spn_list_pad_w.value(),
+			self._spn_list_pad_h.value(),
+			self._spn_list_pad_w.value(),
+			self._spn_list_pad_h.value(),
+		)
+
+		self.sig_list_padding_changed.emit(list_padding)
+
+	@QtCore.Slot()
+	def _userChangedToolsFollow(self):
+
+		self.sig_tools_follow_window_changed.emit(self._chk_tools_follow.isChecked())
+
+	@QtCore.Slot()
+	def _userChangedFancyAnimations(self):
+		
+		self.sig_use_fancy_animations_changed.emit(self._chk_fancy_animation.isChecked())
